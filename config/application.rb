dfb4f5e192fe102ff3322690686ce2915cc66a56
yuuasanuma <yutoibusousayuhuu@0723@gmail.com>
2023-06-13 12:10:56 +0900

    ログイン機能実装完了

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,11 @@ module Photo
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
-    config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
     config.generators do |g|
-      
+
       g.assets false
       g.helper false
     end
