47b684a7ac162885bf3d028f49e7c0ec24da7d5d
yuuasanuma <yutoibusousayuhuu@0723@gmail.com>
2023-06-17 22:04:12 +0900

    プロフィール編集機能完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: 'Profile was successfully updated.'
+    else
+      Rails.logger.debug(@user.errors.full_messages)
+      render :edit
+    end
+  end
+  
   private
 
   def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
 
                     before_validation { email.downcase! }
                     has_secure_password
-                    validates :password, length: { minimum: 6 }
+                    validates :password, presence: true, length: { minimum: 6 }, on: :create, allow_blank: true
+
 end
