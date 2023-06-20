6be8d88996c74a00542056978a5f522e9a8ce296
yuuasanuma <yutoibusousayuhuu@0723@gmail.com>
2023-06-20 21:00:43 +0900

    ピクチャーコントローラーのメールコメントアウト

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -18,7 +18,7 @@
       @picture.image.retrieve_from_cache! session[:picture_cache] if session[:picture_cache].present?
       if @picture.save
         session[:picture_cache] = nil
-        PictureMailer.picture_posted(current_user).deliver_now
+        #PictureMailer.picture_posted(current_user).deliver_now
         redirect_to @picture, notice: '写真の投稿が完了しました.'
       else
         render :new
